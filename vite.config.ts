e7e83d2d3287d6d6fe317cdfb913e2af43ec5140
gines <gines@GINESH>
2026-02-17 12:54:37 +0530

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/ginesh-portfolio/" : "/",
   server: {
-    base: "/ginesh-portfolio/",
     host: "::",
     port: 8080,
     hmr: {
